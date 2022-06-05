a471ff9161fa46164e2d7f40437b2604cd565a2f
Bence Király <kiralybencepecs@gmail.com>
2022-06-05 23:01:57 +0200

    Added sharp and Multer + basic API endpoint

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express')
+const multer = require('multer')
+const sharp = require('sharp')
 const app = express()
-const port = 3000
+const upload = multer({ storage: multer.memoryStorage() })
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.post('/api/compress', upload.single('image'), async (req, res) => {
+    const image = await sharp(req.file.buffer)
+        .jpeg({ quality: 50 })
+        .withMetadata()
+        .toBuffer()
+
+    res.contentType('image/jpeg')
+    res.send(image)
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+app.listen(3000)
